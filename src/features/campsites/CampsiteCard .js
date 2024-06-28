727d53f066ce8945e80a8ab1ac1b380f42584d4f
Janneth Gutierrez <jgutier1@gmail.com>
2024-06-28 16:21:19 -0400

    Added notes on how the updated directory works

--- src/features/campsites/CampsiteCard .js
+++ src/features/campsites/CampsiteCard .js
@@ -4,6 +4,10 @@ import { Link } from "react-router-dom";
 const CampsiteCard = ({campsite}) => {
     const {id, image, name} = campsite;
     return (
+        //this will pass the value of id to the link component as a string
+        //here we use a backtick (not quotes) to convert and integer into a string 
+        //this will trigger a route component that we added in the App.js to the campsiteDetailpage for
+        //whatever id was passed in here....
         <Link to={`${id}`}>
             <Card>
                 <CardImg
--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,6 +1,7 @@
 import { Container } from 'reactstrap';
 import CampsitesList from '../features/campsites/CampsitesList';
 
+//here you should see the Campsite list component, not any details.
 const CampsitesDirectoryPage = () => {
     return (
         <Container>
