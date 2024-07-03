9cbbda90771ba3aced814e30b741d22851175822
Janneth Gutierrez <jgutier1@gmail.com>
2024-07-02 20:03:52 -0400

    Added Comment form validation

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,20 +1,19 @@
 import { Col } from 'reactstrap';
 import Comment from './Comment';
 import { selectCommentsByCampsiteId } from './commentsSlice';
+import CommentForm from './CommentForm';
 
-//we're gonna select an array of Comments [selectCommentsByCampsiteId] with the campsiteId property that matches the one
-//that was passed into this component. By passing that campsiteID to the selectCommentsByCampsiteId selector function. 
 const CommentsList = ({ campsiteId }) => {
     const comments = selectCommentsByCampsiteId(campsiteId);
-
-    //we want to check for a valid, non-empty comments
+    
     if(comments && comments.length > 0) {
         return (
             <Col md='5' className='m-1'>
                 <h4>Comments</h4>
                 {comments.map((comment) => {
                     return <Comment key={comment.id} comment={comment}/>;
-                })}
+                })}  
+                <CommentForm>campsiteId: {campsiteId}</CommentForm>              
             </Col>
         );
     }
